4caf5f7a1e1ffda585cd4b67bd48b8faff0b6ecf
jmangz <echou1@binghamton.edu>
2018-05-02 16:14:57 -0400

    added mainmenutemplate: add, clear, quit. shortcut to quit. create add item window

---
+++ main.js
@@ -0,0 +1,67 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+let addWindow;
+
+// Handle create add window.
+function createAddWindow() {
+  // Create new window.
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add shopping list item',
+  });
+  // Load html into window.
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes: true,
+  }));
+}
+// Create menu template.
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add item',
+        click() {
+          createAddWindow();
+        },
+      },
+      {
+        label: 'Clear items',
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
+// Listen for the app to be ready.
+app.on('ready', () => {
+  // Create new window.
+  mainWindow = new BrowserWindow({});
+  // Load html into window.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes: true,
+  }));
+  // Quit app when closed.
+  mainWindow.on('closed', () => {
+    app.quit();
+  });
+  // Build menu from template.
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu.
+  Menu.setApplicationMenu(mainMenu);
+});
