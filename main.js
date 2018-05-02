14bac1d8e6aa75762fd94e0226414c870492703c
jmangz <echou1@binghamton.edu>
2018-05-02 17:54:13 -0400

    added functionality to add list items

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const { app, BrowserWindow, Menu } = electron;
+const {
+  app, BrowserWindow, Menu, ipcMain,
+} = electron;
 
 let mainWindow;
 let addWindow;
@@ -26,6 +28,11 @@ function createAddWindow() {
     addWindow = null;
   });
 }
+// Catch item:add.
+ipcMain.on('item:add', (e, item) => {
+  mainWindow.webContents.send('item:add', item);
+  addWindow.close();
+});
 // Create menu template.
 const mainMenuTemplate = [
   {
